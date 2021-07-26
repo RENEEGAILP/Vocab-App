e3c2f3cc5a68ed7ca5da0f073609398d86cc52ef
RENEEGAILP <22723224+RENEEGAILP@users.noreply.github.com>
2021-07-26 18:05:06 -0400

    Adding registration and login page to first screen

--- Vocab/app/src/main/java/edu/neu/madcourse/vocab/MainActivity.java
+++ Vocab/app/src/main/java/edu/neu/madcourse/vocab/MainActivity.java
@@ -2,7 +2,10 @@ package edu.neu.madcourse.vocab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        Button loginButton = findViewById( R.id.login_button );
+        Button registerButton = findViewById( R.id.register_button );
+
+        loginButton.setOnClickListener( this::onLoginClick );
+        registerButton.setOnClickListener( this::onRegisterClick );
     }
+
+    public void onLoginClick(View view)
+    {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity( intent );
+    }
+
+    public void onRegisterClick(View view)
+    {
+        Intent intent = new Intent(this,RegistrationActivity.class);
+        startActivity( intent );
+    }
+
 }
